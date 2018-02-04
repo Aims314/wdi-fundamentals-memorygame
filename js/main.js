73d59dd2553a5a25f30719e905966c97a6813b50
Vuong <Amie.Vuong@disney.com>
2018-02-03 16:53:08 -0800

    Memory Game Third Draft

--- js/main.js
+++ js/main.js
@@ -11,28 +11,50 @@ console.log("Up and running!");
 
 // console.log("User flipped " + cardThree);
 
-var cards = ["queen", "queen", "king", "king"]
+var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+//var cardOne = cards[0];
 
-cardsInPlay.push(cardOne);
+//cardsInPlay.push(cardOne);
 
-console.log(cardOne);
+//console.log("User flipped " + cardOne);
 
-console.log("User flipped " + cardOne);
+//var cardTwo = cards[2];
 
-var cardTwo = cards[2];
+//cardsInPlay.push(cardTwo);
 
-cardsInPlay.push(cardTwo);
+//console.log("User flipped " + cardTwo);
 
-console.log("User flipped " + cardTwo);
+//if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+	//alert("You got a Match!");
+//}
 
-if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
+//else {
+	//alert("No match found!");
+//}
+
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!"); 
+	}
+	else {
+	console.log("Sorry, try again.");}
+};
+
+var flipCard = function(cardID){
+	console.log("User flipped " + cards[cardID]);
+	if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
 	alert("You got a Match!");
-}
-
-else {
+	}
+	else {
 	alert("No match found!");
-}
+	}
+	cardsInPlay.push(cards[cardID])
+	
+};
+checkForMatch();	
+flipCard(0);
+flipCard(2);
+
